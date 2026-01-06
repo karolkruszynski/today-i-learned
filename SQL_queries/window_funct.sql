a6c6c15bf92a6b4f7fed384eb777642757399b8e
Karol Kruszyński <90861840+karolkruszynski@users.noreply.github.com>
2026-01-06 18:00:10 +0100

    more window funct

--- SQL_queries/window_funct.sql
+++ SQL_queries/window_funct.sql
@@ -1,51 +1,5 @@
 SELECT
 	customer_id,
-	first_name,
-	last_name,
-	date_added
-FROM
-	customers
-ORDER BY
-	date_added;
-
-SELECT
-	date_added,
-	COUNT(*)
-FROM
-	customers
-GROUP BY
-	date_added
-ORDER BY
-	date_added;
-
--- WINDOW FUNCTION
-SELECT
-	customer_id,
-	title,
-	first_name,
-	last_name,
-	gender,
-	COUNT(*) OVER (PARTITION BY gender) as total_customers
-FROM
-	customers
-ORDER BY
-	customer_id;
-
-SELECT
-	customer_id,
-	title,
-	first_name,
-	last_name,
-	gender,
-	COUNT(*) OVER (ORDER BY customer_id) as total_customers
-FROM
-	customers
-ORDER BY
-	customer_id;
-
-
-SELECT
-	customer_id,
 	title,
 	first_name,
 	last_name,
@@ -57,12 +11,7 @@ FROM
 ORDER BY
 	customer_id;
 
-
--- Użyj funkcji okna i napisz kwerendę, która zwraca
--- informacje o klientach oraz
--- liczbę osób, które podały swój adres.
--- Ponadto uporządkuj listę według dat.
-
+--
 WITH daily_rolling_count as
 (SELECT
 	customer_id,
@@ -91,36 +40,183 @@ FROM
 ORDER BY
 	date_added;
 
+--
+SELECT
+	customer_id,
+	title,
+	first_name,
+	last_name,
+	gender,
+	COUNT(*) OVER w as total_customers,
+	SUM(
+		CASE
+			WHEN title IS NOT NULL THEN 1
+			ELSE 0
+		END
+	) OVER w as total_customers_title
+FROM
+	customers
+WINDOW w as (
+	PARTITION BY gender ORDER BY customer_id
+)
+ORDER BY customer_id;
 
+--
+SELECT
+	customer_id,
+	first_name,
+	last_name,
+	state,
+	date_added::DATE,
+	RANK() OVER(
+		PARTITION BY state ORDER BY date_added
+	) as cust_rank
+FROM
+	customers
+ORDER BY
+	state, cust_rank;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Moving AVG
+WITH daily_sales as (
+	SELECT
+		sales_transaction_date::DATE,
+		SUM(sales_amount) as total_sales
+	FROM
+		sales
+	GROUP BY
+		sales_transaction_date::DATE
+),
+moving_average_calculation_7 as (
+	SELECT
+		sales_transaction_date,
+		total_sales,
+		AVG(total_sales) OVER (
+			ORDER BY sales_transaction_date
+			ROWS BETWEEN 6 PRECEDING and CURRENT ROW
+		) as sales_moving_average_7,
+		ROW_NUMBER() OVER (
+			ORDER BY sales_transaction_date
+		) as row_number
+	FROM
+		daily_sales
+	ORDER BY 1
+)
+SELECT
+	sales_transaction_date,
+	CASE
+		WHEN row_number>=7 THEN sales_moving_average_7
+		ELSE NULL
+	END as sales_moving_average_7
+FROM
+	moving_average_calculation_7;
 	
+--
+WITH daily_sales as (
+	SELECT
+		sales_transaction_date::DATE,
+		SUM(sales_amount) as total_sales
+	FROM
+		sales
+	GROUP BY
+		sales_transaction_date::DATE
+),
+sales_stats_30 as (
+	SELECT
+		sales_transaction_date,
+		total_sales,
+		MAX(total_sales) OVER (
+			ORDER BY sales_transaction_date
+			ROWS BETWEEN 30 PRECEDING and 1 PRECEDING
+		) as max_sales_30
+	FROM
+		daily_sales
+	ORDER BY
+		1
+)
+SELECT
+	sales_transaction_date,
+	total_sales,
+	max_sales_30
+FROM
+	sales_stats_30
+WHERE
+  total_sales > max_sales_30 
+AND
+  sales_transaction_date>='2019-01-01';
+
+--
+WITH daily_sales as (
+	SELECT
+		sales_transaction_date::DATE,
+		SUM(sales_amount) as total_sales
+	FROM
+		sales
+	WHERE sales_transaction_date::date >= DATE '2021-01-01'
+    AND sales_transaction_date::date <  DATE '2022-01-01'
+	GROUP BY
+		sales_transaction_date::DATE
+	ORDER BY
+		1
+),
+moving_avg_30 as (
+	SELECT
+		sales_transaction_date,
+		AVG(total_sales) OVER (
+			ORDER BY sales_transaction_date
+			ROWS BETWEEN 29 PRECEDING and CURRENT ROW
+		) as moving_avg_30,
+		SUM(total_sales) OVER (
+			ORDER BY sales_transaction_date
+			ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+		) as rolling_30_sum,
+		COUNT(*) OVER (
+			ORDER BY sales_transaction_date
+			ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+		) as window_days
+	FROM
+		daily_sales
+	ORDER BY
+		1
+)
+SELECT
+	sales_transaction_date,
+	window_days,
+	CASE
+		WHEN window_days = 30 THEN moving_avg_30
+	END as moving_avg_30,
+	rolling_30_sum,
+	NTILE(10) OVER (ORDER BY rolling_30_sum DESC) AS decile
+FROM
+	moving_avg_30
+WHERE 
+  window_days = 30
+ORDER BY 
+  decile;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
